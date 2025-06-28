317dda470879f0b87d4bf4371caeb8f372ecce1a
michaelfuchaka <fuchakamichael06@gmail.com>
2025-06-28 21:49:40 +0300

    Complete palindrome lab

--- index.js
+++ index.js
@@ -23,3 +23,59 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
+function isPalindrome(word) {
+  return word === reverseString(word);
+}
+
+/* 
+  Pseudocode:
+  - Reverse the input string
+  - Compare it with the original string
+  - Return true if they match, else return false
+*/
+
+/*
+  Explanation:
+  We reverse the input string using built-in methods (split, reverse, join).
+  If the reversed string is exactly the same as the original,
+  the function returns true (it's a palindrome). Otherwise, false.
+*/
+
+// Custom tests
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
+
+module.exports = isPalindrome;
+
